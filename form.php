aca3ae295102cdfd2b7e031b5186d522234ec23e
mivanchev <mivanchev@gmail.com>
2016-04-16 13:24:50 +0300

    bootstrapped the form

--- form.php
+++ form.php
@@ -12,21 +12,36 @@
 </head>
 <body>
 <?php  date_default_timezone_set("Europe/Sofia");
-$now =  date('m-d-Y:h:i'); ?>
+$now =  date('m-d-Y | H:i'); ?>
+<div class="container">
 	<form class='form-group'>
+	<div class="row">
+	<div class="col-md-4 col-md-offset-4 col-sm-6 col-xs-10 col-xs-offset-1">
 	<fieldset class="form-group">
 		<div class='form-group'>
-		<label for="exampleInoutName2">Регистрационен номер: <input class="form-control" id="exampleInoutName2"type='text' name='regnumebr'></label>
+		<label for="exampleInoutName2">Регистрационен номер: </label>
+		<input class="form-control" id="exampleInoutName2"type='text' name='regnumebr' required />
 	</fieldset>
+	</div>
+	</div>
+	<div class="row">
+	<div class="col-md-4 col-md-offset-4 col-sm-6 col-xs-10 col-xs-offset-1">
 	<fieldset class="form-group">
-		<label for="exampleInoutName2">Дата: <input type='text' name='date' class="form-control" id="exampleInoutName2" value="<?php echo $now ?>"></label>
+		<label for="exampleInoutName2">Дата: </label>
+		<input type='text' name='date' class="form-control" id="exampleInoutName2" value="<?php echo $now ?>" readonly/>
 	</fieldset>
+	</div>
+	</div>
+	<div class="row">
+	<div class="col-md-4 col-md-offset-4 col-sm-6 col-xs-10 col-xs-offset-1">
 	<fieldset class="form-group">
-	<input type="file" name="pic">
+	<input type="file" name="pic" required />
 	</fieldset>
 		<label for="exampleInoutName2"> <button type="submit" class="btn btn-primary">Направи запис</button></label>
 	</div>
+	</div>
 	</form>
+	</div>
 </body>
 </html>
 
