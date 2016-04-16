8bd38a627e42f81767992ec0c4a41614503bb26d
mivanchev <mivanchev@gmail.com>
2016-04-16 12:04:35 +0300

    Edited merge

--- logform.php
+++ logform.php
@@ -35,10 +35,6 @@
     <input type="submit" value="Влез" class="btn btn-info btn-lg"></input>
     </div>
     </div>
-<<<<<<< HEAD
-=======
-    <button type="submit" class="btn btn-info">Login</button>
->>>>>>> origin/master
   </form>
 </div>
   </body>
